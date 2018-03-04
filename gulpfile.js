c4d28e7b5bb4b8e131757bc524659c42cfd9324e
Ryan Couse <ryan@bigstormstudio.com>
2018-03-03 21:10:29 -0800

    remove compass deps + clean up

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+process.env.DISABLE_NOTIFIER = true;
 var elixir = require('laravel-elixir');
 
 /*
@@ -12,5 +13,5 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('styles.scss');
 });
