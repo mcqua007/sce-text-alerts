f0b3f27153cbe00a3060df2a12c1bf2b992d5340
Ryan Couse <ryan@bigstormstudio.com>
2018-03-03 21:12:05 -0800

    insure data throughout steps

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,7 +7,6 @@ use App\Exceptions\TooManyAttemptsException;
 use App\Http\Requests\IndexRequest;
 use App\Http\Requests\EnrollmentRequest;
 use App\Http\Requests;
-use Illuminate\Cache\RateLimiter;
 use Illuminate\Http\Request;
 use Illuminate\Support\MessageBag;
 
@@ -18,6 +17,14 @@ use Illuminate\Support\MessageBag;
  */
 class MainController extends Controller
 {
+
+    private function verifySessionHasInputs(Array $array, $session) {
+        $inputVerification = array_map(function($key) use ($array, $session)  {
+            return $session->has($key);
+        }, $array);
+        return !in_array(false, $inputVerification);
+    }
+
     /**
      * Index Page
      *
@@ -41,51 +48,50 @@ class MainController extends Controller
      *
      * @param IndexRequest $request
      * @param Account $account
-     * @param RateLimiter $limiter
      * @return $this|\Illuminate\Http\RedirectResponse
      * @throws TooManyAttemptsException
      */
-    public function enrollment(IndexRequest $request, Account $account, RateLimiter $limiter)
+    public function enrollment(IndexRequest $request, Account $account)
     {
-        // How long before the rate limiter resets
-        $decayMinutes = 30; //minuets
 
-        // Get the request unique signature
-        $key = $request->fingerprint();
+         // Get the inputs if in the session and display it in the view
+        $getInput = function() use ($request) {
+            return [
+                'phone' => ($request->session()->has('phone')) ? $request->session()->get('phone') : '',
+                'first_name' => ($request->session()->has('first_name')) ? $request->session()->get('first_name') : '',
+                'last_name' => ($request->session()->has('last_name')) ? $request->session()->get('last_name') : '',
+                'street_number' => ($request->session()->has('street_number')) ? $request->session()->get('street_number') : '',
+                'street_name' => ($request->session()->has('street_name')) ? $request->session()->get('street_name') : '',
+                'zip_code' => ($request->session()->has('zip_code')) ? $request->session()->get('zip_code') : '',
+                'mobile_optin' => ($request->session()->has('mobile_optin')) ? $request->session()->get('mobile_optin') : '',
+                'email' => ($request->session()->has('email')) ? $request->session()->get('email') : '',
+                'email_optin' => ($request->session()->has('email_optin')) ? $request->session()->get('email_optin') : '',
+                'on_peak_alert' => ($request->session()->has('on_peak_alert')) ? $request->session()->get('on_peak_alert') : '',
+                'off_peak_alert' => ($request->session()->has('off_peak_alert')) ? $request->session()->get('off_peak_alert') : ''
+            ];
+        };
 
-        /**
-         * Check if they already hit the limit and throw a exceptions.
-         * we catch the exception and display a view w/ message in app/Exceptions/Handler.php
-         */
-        if ($limiter->tooManyAttempts($key, 5, $decayMinutes)) {
-            throw new TooManyAttemptsException();
+        // MUST be before everything. Check if user trying to skip into this step without a set service_account_number
+        if($request->method() !== 'POST'){
+            //Array of required session keys to display page via Get
+            $requiredInStep = [
+                'service_account_number',
+            ];
+            // Check if they're set in session
+            if($this->verifySessionHasInputs($requiredInStep, $request->session())){
+                // Good render page
+                return view('enrollment')->with(['account' => $account, 'input' => $getInput()]);
+            } else {
+                // Bad redirect
+                $request->session()->flush();
+                return redirect()->route('index');
+            }
         }
 
         // Record all the inputs into the session
-        $request->session()->put($request->all());  
+        $request->session()->put($request->all());
 
-        //Check if user trying to skip into this step without a set service_account_number
-        if(!$request->session()->has('service_account_number')){
-            $request->session()->flush();
-            return redirect()->route('index');
-        }
-
-        // Get the inputs if in the session and display it in the view
-        $input = [
-            'phone' => ($request->session()->has('phone')) ? $request->session()->get('phone') : '',
-            'first_name' => ($request->session()->has('first_name')) ? $request->session()->get('first_name') : '',
-            'last_name' => ($request->session()->has('last_name')) ? $request->session()->get('last_name') : '',
-            'street_number' => ($request->session()->has('street_number')) ? $request->session()->get('street_number') : '',
-            'street_name' => ($request->session()->has('street_name')) ? $request->session()->get('street_name') : '',
-            'zip_code' => ($request->session()->has('zip_code')) ? $request->session()->get('zip_code') : '',
-            'mobile_optin' => ($request->session()->has('mobile_optin')) ? $request->session()->get('mobile_optin') : '',
-            'email' => ($request->session()->has('email')) ? $request->session()->get('email') : '',
-            'email_optin' => ($request->session()->has('email_optin')) ? $request->session()->get('email_optin') : '',
-            'on_peak_alert' => ($request->session()->has('on_peak_alert')) ? $request->session()->get('on_peak_alert') : '',
-            'off_peak_alert' => ($request->session()->has('off_peak_alert')) ? $request->session()->get('off_peak_alert') : ''
-        ];
-
-        return view('enrollment')->with(['account' => $account, 'input' => $input]);
+        return view('enrollment')->with(['account' => $account, 'input' => $getInput()]);
     }
 
     /**
@@ -97,6 +103,30 @@ class MainController extends Controller
      */
     public function verification(EnrollmentRequest $request, Account $account)
     {
+
+        //Check if user trying to skip into this step without a set service_account_number
+        if($request->method() !== 'POST'){
+            //Array of required session keys to display page
+            $requiredInStep = [
+                'phone',
+                'first_name',
+                'last_name',
+                'street_number',
+                'street_name',
+                'zip_code',
+                'mobile_optin',
+                'service_account_number',
+            ];
+            // Check if they're set in session
+            if($this->verifySessionHasInputs($requiredInStep, $request->session())){
+                // Good render page
+                return view('verification')->with('account', $account);
+            } else {
+                // Bad redirect
+                $request->session()->flush();
+                return redirect()->route('index');
+            }
+        }
 
         // Make sure that session is also updated when false 0 values are passed
         if ( $request->input('mobile_optin') != 1){
@@ -115,13 +145,6 @@ class MainController extends Controller
         // Record all the inputs into the session
         $request->session()->put($request->all());
 
-
-        //Check if user trying to skip into this step without a set service_account_number
-        if(!$request->session()->has('service_account_number') || !$request->session()->has('phone') ){
-            $request->session()->flush();
-            return redirect()->route('index');
-        }
-
         // Everything checks out so we display the verification page
     	return view('verification')->with('account', $account);
     }
@@ -135,10 +158,29 @@ class MainController extends Controller
      */
     public function confirmation(Request $request, Account $account)
     {
-        //Check if user trying to skip into this step without a set service_account_number or phone
-        if($request->method() !== 'POST' || !$request->session()->has('service_account_number') || !$request->session()->has('phone') ){
-            $request->session()->flush();
-            return redirect()->route('index');
+
+        //Check if user trying to skip into this step without a set service_account_number
+        if($request->method() !== 'POST'){
+            //Array of required session keys to display page
+            $requiredInStep = [
+                'phone',
+                'first_name',
+                'last_name',
+                'street_number',
+                'street_name',
+                'zip_code',
+                'mobile_optin',
+                'service_account_number',
+            ];
+            // Check if they're set in session
+            if($this->verifySessionHasInputs($requiredInStep, $request->session())){
+                // Good render page
+                return view('confirmation')->with('account', $account);
+            } else {
+                // Bad redirect
+                $request->session()->flush();
+                return redirect()->route('index');
+            }
         }
 
 
@@ -148,23 +190,26 @@ class MainController extends Controller
         // Get all the session data and update the model with it
         $account = $account->fill($request->session()->all());
 
-        // Save the model.
-        $account->save();
-
+        if (!$request->session()->has('saved')) {
+            // Save the model.
+            $account->save();
+            $request->session()->put('saved', true);
+        }
 
         // Clear inputs stored in session
-        $request->session()->forget('service_account_number');
-        $request->session()->forget('first_name');
-        $request->session()->forget('last_name');
-        $request->session()->forget('street_number');
-        $request->session()->forget('street_name');
-        $request->session()->forget('zip_code');
-        $request->session()->forget('phone');
-        $request->session()->forget('mobile_optin');
-        $request->session()->forget('email');
-        $request->session()->forget('email_optin');
-        $request->session()->forget('on_peak_alert');
-        $request->session()->forget('off_peak_alert');
+        // $request->session()->forget('service_account_number');
+        // $request->session()->forget('first_name');
+        // $request->session()->forget('last_name');
+        // $request->session()->forget('street_number');
+        // $request->session()->forget('street_name');
+        // $request->session()->forget('zip_code');
+        // $request->session()->forget('phone');
+        // $request->session()->forget('mobile_optin');
+        // $request->session()->forget('email');
+        // $request->session()->forget('email_optin');
+        // $request->session()->forget('on_peak_alert');
+        // $request->session()->forget('off_peak_alert');
+        // $request->session()->flush();
 
 
         return view('confirmation')->with('account', $account);
