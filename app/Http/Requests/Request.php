7b58b4bb76c0e126349980d2966eb1fc75010d0f
Andrew Fontes <andrewfontes@gmail.com>
2018-03-09 18:19:12 -0800

    make page drop to anchor ID upon returning with validation errors

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -4,7 +4,18 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
+
+
 abstract class Request extends FormRequest
 {
-    //
+    public function response(array $errors)
+    {
+        if ($this->ajax() || $this->wantsJson()) {
+            return new JsonResponse($errors, 422);
+        }
+
+        return $this->redirector->to($this->getRedirectUrl().'#error-box')
+                                        ->withInput($this->except($this->dontFlash))
+                                        ->withErrors($errors, $this->errorBag);
+    }
 }
--- resources/views/partials/errors.blade.php
+++ resources/views/partials/errors.blade.php
@@ -1,5 +1,5 @@
 @if (count($errors) > 0)
-    <div class="alert alert-danger">
+    <div id="error-box" class="alert alert-danger">
         <ul class="bullet-list">
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
